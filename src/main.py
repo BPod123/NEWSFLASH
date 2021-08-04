d3f2c4c121ed68772d736b54836abcbf67f3fc6f
Ben Podrazhansky <benpodrazhansky@gmail.com>
2021-08-04 15:06:40 -0400

    Added *.csv to .gitignore

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from update_database import updateAllHeadlines
-if __name__ == '__main__':
-    # Get file path to webdriver
-    f = open("../webdriverpath.txt", "r")
-    wdpath = f.read()
-    f.close()
-    del f
-    updateAllHeadlines(wdpath)
