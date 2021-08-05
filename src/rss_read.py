ebdc96359fb642b0a60a74a8412ca3740cb371f4
Ben Podrazhansky <benpodrazhansky@gmail.com>
2021-08-05 14:09:30 -0400

    Checks all feeds now. Also logging

--- src/rss_read.py
+++ src/rss_read.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import time
 from datetime import timedelta, datetime
@@ -7,21 +8,20 @@ import numpy as np
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions as Options
+import threading, queue
+import sys
+webdriver_path = [""]
+output_directory = [""]
+sources_path = [""]
 
-
-def parse_batches(sources: pd.DataFrame):
-    """
-    Reads the RSS Feeds from different sources and converts the batches from each source to a dataframe
-    :param sources:
-    :return:
-    """
-    return np.array([Batch(sources, i) for i in range(len(sources))])
-
-
-def Batch(sources: pd.DataFrame, index_row: int):
-    source_name, rss_url = sources['NAME'][index_row], sources['RSS_URL'][index_row]
-    # timetuple for time struct
-
+update_log = queue.Queue()
+error_log = queue.Queue()
+def dequeue_logs(log_queue, file_path):
+    while True:
+        item = log_queue.get()
+        log(item, file_path)
+        log_queue.task_done()
+def Batch(rss_url):
     feed = feedparser.parse(rss_url)
 
     if feed.bozo:
@@ -49,7 +49,9 @@ class EntryInfo(object):
         else:
             published_parsed = entry['published_parsed']
         title = entry['title']
-        summary = entry['summary']
+
+        summary = entry['summary'] if 'summary' in entry else ""
+
         if '<img ' in summary:
             summary = summary[:summary.index('<img ')]
         if '\n' in summary:
@@ -72,15 +74,10 @@ def extractFeedspotHeadlines(rss_url):
     :param rss_url: string
     :return: numpy array of Entries
     """
-    f = open("../webdriverpath.txt", "r")
-    webdriver_path = f.read()
-    f.close()
-    del f
     ops = Options()
     ops.add_argument('--headless')
-    driver = webdriver.Firefox(executable_path=webdriver_path, options=ops)
+    driver = webdriver.Firefox(executable_path=webdriver_path[0], options=ops, service_log_path='NUL')
     driver.get(rss_url)
-    titles, summaries, publish_dates = [], [], []
     titles = np.array([x.get_attribute('innerHTML') for x in driver.find_elements_by_class_name('ext_link') if
                        len(x.get_attribute('innerHTML'))])
     summaries = np.array([x[:x.index(' ..<a rel=')] for x in
@@ -109,21 +106,28 @@ def parse_datetime(element):
         elif index == 2:
             date -= timedelta(minutes=num)
         else:
-            print("Unknown Date:\n{0}".format(ago))
-    return date
+            error_log.put("parse_datetime error: Unknown Date: \"{0}\" Using datetime.now() instead".format(ago))
+    elif index_str == "M":
+        num = int("".join([x for x in ago if x.isalnum() and not x.isalpha()]))
+        if date.month - num > 0:
+            date = datetime(year=date.year, month=date.month - 1, day=date.day)
+        else:
 
-    #
-    #
-    # spans = element.get_attribute('innerHTML').split("<span")
-    # target = [s for s in spans if " ago" in s][0]
-    # target = [s for s in target.split("-")]
+            date = datetime(year=date.year - 1, month=int(12 - abs(date.month - num)), day=date.day)
+    return date
 
 
 def get_fname(source_str, pub_year, most_recent_batch=False):
+    """
+    :param source_str: Name of source
+    :param pub_year: Year designation, if not a Last Batch File
+    :param most_recent_batch: If true, will ignore year and will return file name for Last Batch
+    :return: The requested file name, either the year file name for a source or the Last Batch file name
+    """
     if not most_recent_batch:
-        return os.path.abspath("../output/{0} {1}.csv".format(pub_year, source_str))
+        return os.path.abspath("{0}/{1} {2}.csv".format(output_directory[0], pub_year, source_str))
     else:
-        return os.path.abspath("../output/Last Batch {0}.csv".format(source_str))
+        return os.path.abspath("{0}/Last Batch {1}.csv".format(output_directory[0], source_str))
 
 
 def insert_batch(source_name, untrimmed_dates: np.ndarray, untrimmed_titles: np.ndarray,
@@ -135,17 +139,17 @@ def insert_batch(source_name, untrimmed_dates: np.ndarray, untrimmed_titles: np.
     :param dates
     :param titles
     :param summaries
-    :return:
+    :return: Number of new sources
     """
 
     dates, titles, summaries, overlap, download_times = trim_batch(source_name, untrimmed_dates, untrimmed_titles,
                                                                    untrimmed_summaries)
 
     if 0 in [len(dates), len(titles), len(summaries)]:
-        return
+        return 0, 0
     years = np.unique(np.vectorize(lambda x: x.year)(dates))
     year_mask = lambda year: (dates >= datetime(year, 1, 1)) & (dates < datetime(year + 1, 1, 1))
-
+    num_new_sources = 0
     for year in years:
         mask = year_mask(year)
         batch_dates, batch_titles, batch_summaries = dates[mask], titles[mask], summaries[mask]
@@ -154,33 +158,33 @@ def insert_batch(source_name, untrimmed_dates: np.ndarray, untrimmed_titles: np.
         fname = get_fname(source_name, year)
 
         df = pd.DataFrame({'DATE': batch_dates, 'TITLE': batch_titles, 'SUMMARY': batch_summaries})
-        if not os.path.isfile(fname):
-            df.to_csv(fname, index=False)
-        else:
-            df.to_csv(fname, header=False, index=False, mode='a')
+        if len(df) > 0:
+            num_new_sources += len(df)
+            if not os.path.isfile(fname):
+                df.to_csv(fname, index=False)
+            else:
+                df.to_csv(fname, header=False, index=False, mode='a')
         del year, df, fname, batch_dates, batch_titles, batch_summaries
     # Now to write the Last Batch file for the next check
     # if there was any overlap, those overlapping rows need to be in the Last Batch file because there is
     # risk of them being read again on the next read
-    ovelap_len = len(overlap)
     if len(overlap) > 0:
         last_batch = (np.concatenate([dates, np.take(untrimmed_dates, overlap)]),
                       np.concatenate([titles, np.take(untrimmed_titles, overlap)]),
                       np.concatenate([summaries, np.take(untrimmed_summaries, overlap)]),
                       np.concatenate([np.full(dates.shape, datetime.now()), download_times]))
-        del dates, titles, summaries, overlap, download_times
+        del dates, titles, summaries, download_times
         dates, titles, summaries, download_times = last_batch
         del last_batch
     else:
         download_times = np.full(dates.shape, datetime.now())
-    try:
-        possible_overlap_batch = pd.DataFrame({'DOWNLOAD_TIME': pd.Series(download_times, dtype=download_times.dtype),
-                                               'DATE': pd.Series(dates, dtype=dates.dtype), 'TITLE': titles,
-                                               'SUMMARY': summaries})
-    except:
-        z = 3
+    possible_overlap_batch = pd.DataFrame({'DOWNLOAD_TIME': pd.Series(download_times, dtype=download_times.dtype),
+                                           'DATE': pd.Series(dates, dtype=dates.dtype), 'TITLE': titles,
+                                           'SUMMARY': summaries})
     possible_overlap_batch.index.name = 'INDEX'
-    possible_overlap_batch.to_csv(get_fname(source_name, None, most_recent_batch=True))
+    if len(possible_overlap_batch) > 0:
+        possible_overlap_batch.to_csv(get_fname(source_name, None, most_recent_batch=True))
+    return num_new_sources, len(overlap)
 
 
 def trim_batch(source_name, dates: np.ndarray, titles: np.ndarray, summaries: np.ndarray):
@@ -223,9 +227,6 @@ def trim_batch(source_name, dates: np.ndarray, titles: np.ndarray, summaries: np
             reader.close()
             del reader, col_dict, range_arr, item
 
-    # title_skip_rows = set(np.unique(np.concatenate(title_skip_rows)))
-    # summary_skip_rows = set(np.unique(np.concatenate(summary_skip_rows)))
-    # overlap = np.array(list(title_skip_rows.intersection(summary_skip_rows)))
     overlap = title_skip_rows.intersection(summary_skip_rows)
     idx_overlap = title_index_skip_rows.intersection(summary_index_skip_rows)
     del title_skip_rows, summary_skip_rows, skip_rows, idx_skip_rows, title_index_skip_rows, summary_index_skip_rows
@@ -244,23 +245,77 @@ def trim_batch(source_name, dates: np.ndarray, titles: np.ndarray, summaries: np
     return new_dates, new_titles, new_summaries, overlap, download_times
 
 
-def check_source(sources, i):
-    dates, titles, summaries = Batch(sources, i)
-    insert_batch(sources['NAME'][i], dates, titles, summaries)
+def check_source(source_name, rss_feed):
+    dates, titles, summaries = Batch(rss_feed)
+    num_new_sources = insert_batch(source_name, dates, titles, summaries)
+    return num_new_sources
+
+
+def run_threads(source_names, rss_urls, intervals):
+    while True:
+        with ThreadPoolExecutor() as executor:
+            try:
+                # futures = [executor.submit(run_threads, interval_sources[interval][0], interval_sources[interval][1]) for interval in interval_sources]
+                futures = [executor.submit(run_thread, source_names[i], rss_urls[i],
+                                           intervals[i]) for i in range(len(source_names))]
+            except Exception as error:
+                error_log.put("{0} Executor Failed\nError Message: {1}".format(datetime.now(), error))
+            finally:
+                # del interval_sources
+                executor.shutdown()
+
+
+def run_thread(source_name, rss_url, interval):
+    while True:
+        try:
+            new_sources, num_overlap = check_source(source_name, rss_url)
+            update_log.put("{0}\t{1}\t{2}\t{3}".format(datetime.now(), source_name, new_sources, num_overlap))
+        except Exception as error:
+            error_log.put("{0} FAILED: {1}\nError Message{2}".format(datetime.now(), source_name, error))
+            time.sleep(10)
+            continue
+        time.sleep(interval)
+
+def log(string, file_path):
+    sys.stdout = open(file_path, 'a')
+    print(string)
+    sys.stdout.close()
+
+        
+
+def chunk_list(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+    return out
+
+
+
+def main():
+    f = open("../config.txt", "r")
+    lines = f.read().split("\n")
+    f.close()
+
+    webdriver_path[0] = lines[0]
+    output_directory[0] = lines[1]
+    sources_path[0] = lines[2]
+    update_log_path = lines[3]
+    error_log_path = lines[4]
+
+    updater_thread = threading.Thread(target=dequeue_logs, args=(update_log, update_log_path), daemon=True)
+    updater_thread.start()
+    error_thread = threading.Thread(target=dequeue_logs, args=(error_log, error_log_path),daemon=True)
+    sources = pd.read_csv(sources_path[0])
+
+
+    [threading.Thread(target=run_thread,
+                      args=(sources['NAME'][i], sources['RSS_URL'][i], sources['PUBLISH_FREQUENCY'][i])).start() for i in range(len(sources))]
 
 
 if __name__ == '__main__':
-    # TODO make separate threads for each source that sleep different amount of times
-    # Also, updates Sources.csv to have the frequency at which sources are checked
-    # When setting up the threads, run each batch update once, then schedule them to update at different times.
+    main()
 
-    sources = pd.read_csv('Sources.csv')
-    while(True):
-        start = datetime.now()
-        with ThreadPoolExecutor() as executor:
-            futures = [executor.submit(check_source, sources, i) for i in range(len(sources))]
-            executor.shutdown()
-        end = datetime.now()
-        print("Download Completed at {0}. Time Taken: {1}".format(datetime.now(), end - start))
-        del start, end
-        time.sleep(10800) # 10,800 seconds in 3 hours
+
