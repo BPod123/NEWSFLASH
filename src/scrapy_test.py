9abcc1e109738fec3da5222411494c8def56bf8c
Ben Podrazhansky <benpodrazhansky@gmail.com>
2022-12-14 10:39:03 -0500

    Tests all feedspot feeds

--- src/scrapy_test.py
+++ src/scrapy_test.py
@@ -31,8 +31,63 @@ def run_test(rss_url):
         publish_dates.append(pub_time_info)
     return np.array(publish_dates), np.array(titles), np.array(summaries)
 def start_test():
-    results = run_test("https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5261952&q=site:")
-    print(f"Results\n{results}")
+    urls = [
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5261952&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5261947&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5261944&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5245958&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5245948&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5245935&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5245929&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5245912&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5243665&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5243663&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5243652&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5243650&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5243646&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5242090&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5242079&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5242057&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5242033&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=4555082&q=site:https%3A%2F%2Fwww.theatlantic.com%2Ffeed%2Fchannel%2Fpolitics%2F",
+        "https://www.feedspot.com/infiniterss.php?_src=feed_title&followfeedid=5244063&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=feed_title&followfeedid=5244061&q=site:",
+        "https://www.feedspot.com/infiniterss.php?_src=feed_title&followfeedid=5232981&q=site:https%3A%2F%2Fwww.theatlantic.com%2Ffeed%2Fall%2F"
+    ]
+    names = [
+        "The Atlantic Technology",
+        "The Atlantic Global",
+        "The Atlantic Business",
+        "NBC Science",
+        "NBC World",
+        "NBC Politics",
+        "NBC US",
+        "NBC Coronavirus",
+        "Sky News Business",
+        "Sky News Technology",
+        "Sky News Political News",
+        "Sky News World News",
+        "Sky News UK",
+        "NPR Politics",
+        "NPR Europe",
+        "NPR Asia",
+        "NPR National",
+        "The Atlantic Politics",
+        "FOX Business Markets",
+        "FOX Business Economy",
+        "The Atlantic World"
+    ]
+    fails = []
+    for name, url in zip(names, urls):
+        print("Running Test for {name}: {url}")
+        results = run_test(url)
+        if any((len(x) == 0 for x in results)):
+            fails.append((name, url))
+    if len(fails) > 0:
+        print(f"{len(fails)} tests failed:")
+        for i, (name, url) in enumerate(fails, start=1):
+            print(f"{i}) {name} - {url}")
+    else:
+        print("All tests passed")
 if __name__ == '__main__':
-    results = run_test("https://www.feedspot.com/infiniterss.php?_src=followbtn&followfeedid=5261952&q=site:")
-    print(f"Results\n{results}")
+    start_test()
