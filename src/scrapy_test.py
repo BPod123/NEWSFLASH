5c698260cd3faeded12922a82cd1cca73b42a3eb
Ben Podrazhansky <benpodrazhansky@gmail.com>
2022-12-14 10:25:26 -0500

    More print statements

--- src/scrapy_test.py
+++ src/scrapy_test.py
@@ -15,18 +15,24 @@ def run_test(rss_url):
     for i, string in enumerate(list(map(str,map(Selector.get, entry_items))), start=1):
         print(f"\n\n\n\n{i})")
         print(string)
-    for entry in entry_items:
+    for i, entry in enumerate(entry_items, start=1):
+        print(f"{i})")
         title_link = entry.css(".entry__item_title").css("a")[0].get()
         title = re.findall(r"(?<=>).*(?=</a>)", title_link)[0]
+        print(f"title: {title}")
         pub_time_info = parse_datetime(entry.css(".entry__item_time").xpath("text()").get().strip())
+        print(f"Publish time: {pub_time_info}")
         excerpt = entry.css(".entry__item_excerpt").xpath("text()").get().strip()
         if excerpt.endswith(".."):
             excerpt = excerpt[:-2]
+        print(f"Excerpt: {excerpt}")
         titles.append(title)
         summaries.append(excerpt)
         publish_dates.append(pub_time_info)
     return np.array(publish_dates), np.array(titles), np.array(summaries)
 def start_test():
-    run_test('https://www.feedspot.com/infiniterss.php?_src=feed_title&followfeedid=5244063&q=site:')
+    results = run_test('https://www.feedspot.com/infiniterss.php?_src=feed_title&followfeedid=5244063&q=site:')
+    print(f"Results\n{results}")
 if __name__ == '__main__':
-    run_test('https://www.feedspot.com/infiniterss.php?_src=feed_title&followfeedid=5244063&q=site:')
+    results = run_test('https://www.feedspot.com/infiniterss.php?_src=feed_title&followfeedid=5244063&q=site:')
+    print(f"Results\n{results}")
